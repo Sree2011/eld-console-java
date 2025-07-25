3172fcc2406c27fe0d3840960003ad241fc92b55
Sree Sai Nandini Gundraju <gundrajusreesainandini@gmail.com>
2025-07-25 19:50:32 +0530

    csv functionality

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,27 @@
+
 public class Main{
     public static void main(String[] args) {
-    Generator[] generators = new Generator[]{
-    new Generator(1, 125, 5000, 0.02f, 45f, 0.0003f),
-    new Generator(2, 7, 300, 0.03f, 50f, 0.0035f),
-    new Generator(3, 9, 358, 0.025f, 52f, 0.0030f),
-    new Generator(4, 7, 286, 0.03f, 48f, 0.0029f),
-    new Generator(5, 6, 260, 0.035f, 55f, 0.0054f),
-    new Generator(6, 8, 348, 0.02f, 50f, 0.0022f),
-    new Generator(7, 6, 264, 0.027f, 51f, 0.0032f),
-    new Generator(8, 6, 243, 0.03f, 53f, 0.0028f),
-    new Generator(9, 8, 345, 0.025f, 47f, 0.0030f),
-    new Generator(10, 10, 420, 0.015f, 40f, 0.0012f)
-};
+        Generator[] genlist = InputLoader.loadFromCSV("input/10-generator_system.csv").toArray(new Generator[0]);
 
+
+        // ArrayList<Generator> genlist = InputLoader.loadFromUser();
+        if (genlist.length == 0) {
+            System.out.println("No valid generators loaded. Exiting.");
+            return;
+        }
+
+       // Generator[] genlist = genlist.toArray(new Generator[0]);
+        System.out.println("Total generators loaded: " + genlist.length);
+        System.out.printf("%-12s %-15s %-15s %-12s %-12s %-12s%n", 
+                  "Gen ID", "Min Capacity", "Max Capacity", "a", "b", "c");
+        System.out.println("--------------------------------------------------------------------------");
+        for (Generator gen : genlist) {
+            System.out.printf("%-12d %-15d %-15d %-12.3f %-12.3f %-12.3f%n",
+                            gen.getGen_id(), gen.getMin_capacity(), gen.getMax_capacity(),
+                            gen.getA(), gen.getB(), gen.getC());
+        }
     float totalDemand = 2000f;
-        ELDCalculator eldCalculator = new ELDCalculator(generators.length, generators, totalDemand);
+        ELDCalculator eldCalculator = new ELDCalculator(genlist.length, genlist, totalDemand);
         
         float[] ELDispatch = eldCalculator.lambdaIteration();
         for(int i = 0; i < ELDispatch.length; i++) {
